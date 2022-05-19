ebb3a7dbdbcd5fe03939f6ff0b0be3954514a4e5
panasabena <62650626+panasabena@users.noreply.github.com>
2022-05-19 01:02:22 -0300

    Add files via upload

---
+++ Ejercicio clase 1.sql
@@ -0,0 +1,12 @@
+Select * from system_user;
+select first_name 
+from system_user;
+select id_system_user, first_name, last_name
+from system_user;
+select id_system_user, first_name, email
+from system_user;
+select id_system_user, first_name, last_name
+from system_user
+WHERE id_system_user = 56;
+SELECT * FROM system_user WHERE first_name = 'Gillie';
+SELECT * FROM system_user WHERE first_name = 'Reinaldos';
