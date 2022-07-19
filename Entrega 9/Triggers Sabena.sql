644901dcb02d6f8335dbf5fc32a4c054cb60b8c4
panasabena <alfre_sabena@hotmail.com>
2022-07-19 18:17:56 -0300

    Add files via upload

--- Entrega 9/Triggers Sabena.sql
+++ Entrega 9/Triggers Sabena.sql
@@ -1,11 +1,15 @@
 USE HOSPITAL;
 
-/*Creando tabla para el trigger*/
+/*Creando tabla para el trigger "Before" */
+drop table if exists cita_cancelada;
+
+/*Tener cuidado con esta tabla, porque la fecha y la hora están en formato texto
+entonces cuando creamos otra tabla trigger tiene que tener el mismo formato*/
 CREATE TABLE `CITA_CANCELADA` (
   `Id_Cita` int NOT NULL,
   `Id_Paciente` INT NOT NULL,
-  `Fecha_Cita` DATE,
-  `Hora_Cita` DATETIME,
+  `Fecha_Cita` text,
+  `Hora_Cita` text,
   `Id_Medico` INT NOT NULL,
   `Categoria` VARCHAR(255),
   `Descripcion` VARCHAR(255)
@@ -19,7 +23,6 @@ En este caso lo que queremos lograr es que cada cita cancelada se guarde en la t
 canceladas.*/
 
 /*Borro trigger si es que ya está creado*/
-
 drop trigger if exists BEF_DEL_log;
 
 /*Sentencia del trigger*/
@@ -28,7 +31,7 @@ select * from cita;
 CREATE TRIGGER BEF_DEL_log
 BEFORE DELETE ON cita FOR EACH ROW
 
-INSERT INTO CITA_CANCELADA VALUES (Old.Id_cita, Old.Id_Paciente, Old.Fecha_Cita, Old.Hora_Cita
+INSERT INTO CITA_CANCELADA VALUES (Old.Id_cita, Old.Id_Paciente, Old.Fecha_Cita, Old.Hora_Cita,
 Old.Id_Medico, Old.Categoria, Old.Descripcion);
 
 SELECT * FROM CITA_CANCELADA;
@@ -41,16 +44,16 @@ values (Id_cita, Id_Paciente, Fecha_Cita, Hora_Cita, Id_Medico,
 
 select * from cita;
 
-INSERT INTO `CITA` (`Id_Cita`,`Id_Paciente`,`Fecha_Cita`,`Hora_Cita`,`Id_Medico`,`Categoria`,`Descripcion`) VALUES (505,44078271,'7/7/2022','8:00:00 AM',8,'Urgente','Below the national average');
+# INSERT INTO `CITA` (`Id_Cita`,`Id_Paciente`,`Fecha_Cita`,`Hora_Cita`,`Id_Medico`,`Categoria`,`Descripcion`) VALUES (505,44078271,'7/7/2022','8:00:00 AM',8,'Urgente','Below the national average');
 # INSERT INTO `CITA` (`Id_Cita`,`Id_Paciente`,`Fecha_Cita`,`Hora_Cita`,`Id_Medico`,`Categoria`,`Descricpión`) VALUES (506,39176910,'7/22/2022','8:00:00 AM',9,'Leve','Below the national average');
 
-
-DELETE FROM CITA_cancelada;
-
+/*Borramos de la tabla cita el id 505 porque fué cancelado*/
 DELETE FROM CITA WHERE Id_Cita = 505;
 
 select * from CITA;
-select * from CITA_CANCELADA;
+
+## Luego vemos en la tabla citas canceladas que el id_cita = 505 fue cancelado
+select * from CITA_CANCELADA; ## Funciona
 /*--------------------------------------------------------------------------------------*/
 
 /*Creando triger para after*/
